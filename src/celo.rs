89e21e5d94aadb7f8aeed84539c89a03e9ba2db9
pgold <pgold@users.noreply.github.com>
2021-02-28 15:12:54 +0000

    Move celo functions to its own module.

---
+++ src/celo.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use ethers::prelude::abigen;
+use ethers::providers::Middleware;
+use ethers::types::Address;
+use std::sync::Arc;
+
+const REGISTRY_ADDRESS: &str = "000000000000000000000000000000000000ce10";
+
+const EXCHANGE: &str = "Exchange";
+const GOLD_TOKEN: &str = "GoldToken";
+const STABLE_TOKEN: &str = "StableToken";
+
+abigen!(Erc20, "./src/abis/IERC20.json");
+abigen!(Registry, "./src/abis/Registry.json");
+abigen!(Exchange, "./src/abis/Exchange.json");
+
+async fn get_erc20_token<M: Middleware>(client: Arc<M>, name: &str) -> Result<Erc20<M>> {
+    let address = registry_lookup(client.clone(), name).await?;
+    let token = Erc20::new(address, client.clone());
+    Ok(token)
+}
+
+pub async fn get_celo_token<M: Middleware>(client: Arc<M>) -> Result<Erc20<M>> {
+    get_erc20_token(client, GOLD_TOKEN).await
+}
+
+pub async fn get_cusd_token<M: Middleware>(client: Arc<M>) -> Result<Erc20<M>> {
+    get_erc20_token(client, STABLE_TOKEN).await
+}
+
+pub async fn get_exchange<M: Middleware>(client: Arc<M>) -> Result<Exchange<M>> {
+    let exchange_address = registry_lookup(client.clone(), EXCHANGE).await.unwrap();
+    let exchange = Exchange::new(exchange_address, client.clone());
+    Ok(exchange)
+}
+
+pub async fn registry_lookup<M: Middleware>(client: Arc<M>, name: &str) -> Result<Address> {
+    let registry_address: Address = REGISTRY_ADDRESS.parse()?;
+    let registry = Registry::new(registry_address, client);
+    let address = registry
+        .get_address_for_string(name.to_string())
+        .call()
+        .await
+        .unwrap();
+    Ok(address)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
 use anyhow::{anyhow, Context, Result};
-use ethers::prelude::*;
-use ethers::providers::{Http, JsonRpcClient, Provider};
-use ethers::types::Address;
+use ethers::providers::{Http, Middleware, Provider};
+use ethers::types::{Address, U256};
 use std::convert::TryFrom;
 use std::sync::Arc;
 use structopt::StructOpt;
 
-const REGISTRY_ADDRESS: &str = "000000000000000000000000000000000000ce10";
-
-const EXCHANGE: &str = "Exchange";
-const GOLD_TOKEN: &str = "GoldToken";
-const STABLE_TOKEN: &str = "StableToken";
-
-abigen!(Erc20, "./src/abis/IERC20.json");
-
-abigen!(Registry, "./src/abis/Registry.json");
-
-abigen!(Exchange, "./src/abis/Exchange.json");
+mod celo;
 
 #[derive(StructOpt)]
 struct CelophaneOpt {
@@ -60,41 +49,14 @@ enum Command {
     Exchange(ExchangeCommand),
 }
 
-async fn get_erc20_token<T: JsonRpcClient>(
-    client: Arc<Provider<T>>,
-    name: &str,
-) -> Result<Erc20<Provider<T>>> {
-    let address = registry_lookup(client.clone(), name).await?;
-    let token = Erc20::new(address, client.clone());
-    Ok(token)
-}
-
-async fn registry_lookup<T: JsonRpcClient>(
-    client: Arc<Provider<T>>,
-    name: &str,
-) -> Result<Address> {
-    let registry_address: Address = REGISTRY_ADDRESS.parse()?;
-    let registry = Registry::new(registry_address, client);
-    let address = registry
-        .get_address_for_string(name.to_string())
-        .call()
-        .await
-        .unwrap();
-
-    Ok(address)
-}
-
-async fn account_balance<T: JsonRpcClient>(
-    client: Arc<Provider<T>>,
-    args: AccountBalanceOpt,
-) -> Result<()> {
+async fn account_balance<M: Middleware>(client: Arc<M>, args: AccountBalanceOpt) -> Result<()> {
     let address = args
         .address
         .parse::<Address>()
         .with_context(|| "failed to parse account address")?;
 
-    let celo = get_erc20_token(client.clone(), GOLD_TOKEN).await.unwrap();
-    let cusd = get_erc20_token(client.clone(), STABLE_TOKEN).await.unwrap();
+    let celo = celo::get_celo_token(client.clone()).await.unwrap();
+    let cusd = celo::get_cusd_token(client.clone()).await.unwrap();
     let celo_balance = celo.balance_of(address).call().await.unwrap();
     let cusd_balance = cusd.balance_of(address).call().await.unwrap();
 
@@ -104,12 +66,8 @@ async fn account_balance<T: JsonRpcClient>(
     Ok(())
 }
 
-async fn exchange_show<T: JsonRpcClient>(
-    client: Arc<Provider<T>>,
-    args: ExchangeShowOpt,
-) -> Result<()> {
-    let mento_address = registry_lookup(client.clone(), EXCHANGE).await.unwrap();
-    let exchange = Exchange::new(mento_address, client.clone());
+async fn exchange_show<M: Middleware>(client: Arc<M>, args: ExchangeShowOpt) -> Result<()> {
+    let exchange = celo::get_exchange(client.clone()).await.unwrap();
 
     let base_qty = args.amount;
 
